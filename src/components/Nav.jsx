948a75b646c1afe708ded1ef5de222ea379dc61f
colin <42125428+colinhartigan@users.noreply.github.com>
2024-11-27 23:31:28 -0500

    cleanup

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,11 +2,18 @@ import { NavLink } from "react-router";
 
 export default function Nav({}) {
     return (
-        <div className='w-full h-auto flex flex-row justify-center items-center gap-5 p-4'>
-            <NavLink to='/hudson' className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600'>
+        <div className='w-auto h-auto flex flex-row justify-center items-center gap-5 p-4'>
+            <NavLink
+                to='/hudson'
+                className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600 text-right'
+            >
                 Hudson
             </NavLink>
-            <NavLink to='/grill' className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600'>
+            <p>R.</p>
+            <NavLink
+                to='/grill'
+                className=' underline text-blue-600 hover:text-blue-800 visited:text-purple-600 text-left'
+            >
                 Grill
             </NavLink>
         </div>
